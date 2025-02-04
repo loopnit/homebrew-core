d0cccee9f3f2ace1646565ca95a2c9e0ea1a8956
Carlo Cabrera <github@carlo.cab>
2025-02-04 20:41:17 +0100

    chapel: revision bump (switch to `llvm@19`)

--- Formula/c/chapel.rb
+++ Formula/c/chapel.rb
@@ -22,7 +22,7 @@ class Chapel < Formula
   depends_on "gmp"
   depends_on "hwloc"
   depends_on "jemalloc"
-  depends_on "llvm"
+  depends_on "llvm@19"
   depends_on "pkgconf"
   depends_on "python@3.13"
 
